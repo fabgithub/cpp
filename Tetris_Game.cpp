d0a67f6ebeb83a638352fc7c386ff22b6495077b
YongYe <complex.invoke@gmail.com>
2013-11-20 16:44:08 +0800

    Version 1.0.6

--- Tetris_Game.cpp
+++ Tetris_Game.cpp
@@ -8,7 +8,7 @@
 //   Project: https://github.com/yongye/cpp                              //
 //   Project: https://github.com/yongye/shell                            //
 //   Author : YongYe <complex.invoke@gmail.com>                          //
-//   Version: 1.0.6 02/20/2013 BeiJing China [Updated 11/17/2013]        //
+//   Version: 1.0.6 02/20/2013 BeiJing China [Updated 11/20/2013]        //
 //                                                                       //
 //   Algorithm:                                                          //
 //                                                                       //
@@ -145,7 +145,7 @@ int get_args(vector<string>& args)
        }
        else if ( str == "-v" || str == "--version" )
        {
-            cout << "Tetris Game  Version 1.0.6 [Updated 11/17/2013]" << endl;
+            cout << "Tetris Game  Version 1.0.6 [Updated 11/20/2013]" << endl;
             return 1;
        }
        else
@@ -948,7 +948,7 @@ void board::notify()
      cout << "\e["+to_string(toph+17)+";"+to_string(dist)+"HT|t      ===   transpose      Space|enter  ===   drop all down\n";
      cout << "\e[38;5;106;1m\e["+to_string(toph+19)+";"+to_string(dist)+"HTetris Game  Version 1.0.6\n";
      string str8 = "\e["+to_string(toph+20)+";"+to_string(dist)+"HYongYe <complex.invoke@gmail.com>\e[";
-     string str9 = to_string(toph+21)+";"+to_string(dist)+"H02/20/2013 BeiJing China [Updated 11/17/2013]";
+     string str9 = to_string(toph+21)+";"+to_string(dist)+"H02/20/2013 BeiJing China [Updated 11/20/2013]";
      cout << str8+str9 << endl;
 }
 
